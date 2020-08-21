6711a1d54c5dd15eb67f83c7a8545accc5190c9a
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-08-21 14:31:13 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -8,7 +8,8 @@ namespace Kata
         public int Add(string input = "")
         {
             if(String.IsNullOrWhiteSpace(input)) return 0;
-            var numbersList = input.Split(',');
+            var separators = new[] {',', '\n'};
+            var numbersList = input.Split(separators, StringSplitOptions.None);
             return numbersList.Sum(i=> int.Parse(i));
         }
     }
