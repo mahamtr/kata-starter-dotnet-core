dfa4d98d8b777e26af85cb619c284d7362b92ef8
Maai Ham <mzmaai70@gmail.com>
2020-08-21 14:18:30 -0600

    spec#1 Done Pong

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -30,6 +30,19 @@ namespace Kata.Spec
     }
 
     // Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
+    public class when_input_is_single_number
+
+    {
+
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add("3"); };
+
+        It should_return_the_same = () => { _result.Should().Be(3); };
+        private static Calculator _systemUnderTest;
+        private static object _result;
+    }
+    
     // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
     // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
     // Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
     {
-        public int Add()
+        public int Add(string input = "")
         {
+            if(String.IsNullOrWhiteSpace(input)) return 0;
             return -1;
         }
     }
